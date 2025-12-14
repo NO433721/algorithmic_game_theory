ba55801d7e747fe52de0c86be0fc0aba624f21c7
no433721 <chl199912@gmail.com>
2025-12-14 20:32:20 +0100

    afafafqa

--- tests/week07.py
+++ tests/week07.py
@@ -258,6 +258,12 @@ def compute_average_strategy(root_node, strategy_a, strategy_b, alpha, player_id
     
     return avg_strategy
 
+
+def fictitious_play(*args, **kwargs):
+    """Run Extensive-form Fictitious Play for a given number of iterations."""
+
+    raise NotImplementedError
+
             
 def compute_exploitability(*args, **kwargs):
     """Compute and plot the exploitability of a sequence of strategy profiles."""
